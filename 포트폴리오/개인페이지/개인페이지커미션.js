e07321430a2ec26e9ec2c3ff84d5fcf10259130f
koala <koala99678>
2020-02-03 15:33:18 +0900

    20200203

---
+++ 포트폴리오/개인페이지/개인페이지커미션.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+      $('.commission-body-menu .fa-times').click(function(){
+        const allmenu2 =  document.querySelector('.commission-body-menu')
+          allmenu2.classList.add('animated', 'bounceOut')
+          allmenu2.addEventListener('animationend', function() {  
+            $('.commission-body-menu').addClass('display-none')
+            allmenu2.classList.remove('animated', 'bounceOut')
+        })
+        
+        $('#element').off('scroll touchmove mousewheel');
+      })
+      $('.commission-body-menu').on('scroll touchmove mousewheel', function(event) {
+          event.preventDefault();
+          event.stopPropagation();
+          return false;
+        });
+        var swiper2 = new Swiper('.commission-container', {
+            effect: 'flip',
+            grabCursor: true,
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          });
+})
