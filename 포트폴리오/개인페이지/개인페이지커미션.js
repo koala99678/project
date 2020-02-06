27d7b4d6bb1347b3dd23a46347f9819aa5b707bb
koala <koala99678>
2020-02-06 16:18:37 +0900

    20200206

--- 포트폴리오/개인페이지/개인페이지커미션.js
+++ 포트폴리오/개인페이지/개인페이지커미션.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
           event.stopPropagation();
           return false;
         });
-        var swiper = new Swiper('.swiper-container', {
+        var swiper = new Swiper('.commission-typeselect-swiper', {
           effect: 'flip',
           grabCursor: true,
           navigation: {
@@ -22,4 +22,25 @@ $(document).ready(function(){
             prevEl: '.swiper-button-prev',
           },
         });
+        $('[data-toggle="tooltip"]').tooltip();
+        
+        $('.commission-typemenu a').hover(function(){
+          // var linkbox3 =  document.querySelector('.commission-typemenu a')
+          // linkbox3.classList.add('animated', 'pulse','infinite')
+          // 자바 스크립트 코드로 변환하면 아래와 같음
+          $(this).addClass('animated pulse infinite')
+
+        },function(){
+          // var linkbox3 =  document.querySelector('.commission-typemenu a')
+          // linkbox3.classList.remove('animated', 'pulse','infinite')
+          // 자바 스크립트 코드로 변환하면 아래와 같음
+          $(this).removeClass('animated pulse infinite')
+        })
+        $('.commission-typemenu>a').click(function(){
+          if($(this).hasclass('a1')){
+            $('.atype').animate({
+              marginLeft: '500px'
+            })
+          }
+        })
 })
